6e8fa9d4f1eb8bf326cd47a83c9621125509ec2b
Sebastian Moruszewicz <sebasmorus@gmail.com>
2020-12-04 21:33:05 -0600

    Added initial catch test

---
+++ Tests/graph_tests.cpp
@@ -0,0 +1,16 @@
+#include "../cs225/catch/catch.hpp"
+#include "../Graph/Graph.h"
+#include "../Graph/Edge.h"
+
+TEST_CASE("Test Case Name")
+{
+	// Insert code to run here	
+	//...
+	//
+
+	// Require in order to pass case, aborts execution on failure
+	REQUIRE(true);
+
+	// Reuire in order to pass case, continues execution on failure
+	CHECK(true);
+}
