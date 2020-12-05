5e5703e56d65411c8004aafcb2ecf3b8606a3a78
Urish, Adam Walter <adamwu2@illinois.edu>
2020-12-04 22:48:04 -0600

    Add comments ✍️

--- parsing/file_parser.cpp
+++ parsing/file_parser.cpp
@@ -27,9 +27,8 @@ std::vector<string> FileParser::csvline_to_vector(string line){
             ret.push_back(tmp);
             tmp = "";   //Reset tmp after every comma
         }
-        //make it so that this does the thing right
-        else if(c == '\"'){
-            in_quotes = !in_quotes;
+        else if(c == '\"'){ //keep track of quoted text to ignore comments in names
+            in_quotes = !in_quotes; //toggle quotes
         }
         else{
             tmp.push_back(c);   //Add every character of comma seperated entry to tmp string
@@ -41,9 +40,9 @@ std::vector<string> FileParser::csvline_to_vector(string line){
 
 /*Graph*/ void FileParser::airports_to_graph(std::vector<std::vector<string>>* airport_csv, std::vector<std::vector<string>>* routes_csv){
     // Graph g;
-    for(std::vector<string> line : *airport_csv){
-        // g.insertVertex(line[FileParser::iata_idx]);
-        std::cout << line[FileParser::iata_idx] << std::endl;
+    for(std::vector<string> line : *airport_csv){ //for every airport
+        // g.insertVertex(line[FileParser::iata_idx]); // use IATA callsign for vertex name
+        std::cout << line[FileParser::iata_idx] << std::endl; //temporary testing line
     }
     // return g;
 }
