10072ace6385eccb39aa5da7a4599f825f10417a
Sebastian Moruszewicz <sdm6@illinois.edu>
2020-12-07 20:33:14 -0600

    Shows that tests do not compile

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,12 +1,15 @@
 #include "../cs225/catch/catch.hpp"
-/*#include "../graph/graph.h"
+#include "../parsing/file_parser.h"
+#include "../AirGraph/AirGraph.h"
+#include "../graph/graph.h"
 #include "../graph/edge.h"
-*/
+
 TEST_CASE("Test Case Name")
 {
 	// Insert code to run here	
 	//...
 	//
+	AirGraph traffic;
 
 	// Require in order to pass case, aborts execution on failure
 	REQUIRE(true);
